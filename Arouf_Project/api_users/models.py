0fa22a7a883e95d9412816bcb047bcb1496af2dc
sylouan.corfa <sylouan.corfa@epsi.fr>
2019-12-03 16:47:46 +0100

    Json response in get/user

--- Arouf_Project/api_users/models.py
+++ Arouf_Project/api_users/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
+    id = models.IntegerField(primary_key=True)
     nom = models.TextField(max_length=30)
     prenom = models.TextField(max_length=30)
-    username = models.TextField(max_length=30)
-    email = models.TextField(max_length=30)
+    username =models.TextField(max_length=30)
+    email=models.TextField(max_length=30)
--- Arouf_Project/api_users/views.py
+++ Arouf_Project/api_users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
+from django.core import serializers
+
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -9,18 +11,20 @@ from django.views.decorators.csrf import csrf_exempt
 def getAllUsers(request):
     queryset = User.objects.all().order_by('prenom')
     if queryset:
-        return HttpResponse(queryset)
+        qs_json = serializers.serialize('json', queryset)
+        return HttpResponse(qs_json, content_type='application/json')
     else:
         return HttpResponse("Aucun utilisateur enregistré")
 
 @csrf_exempt
 def insertUser(request):
     try:
+        id = request.POST["id"]
         name = request.POST['name']
         surname = request.POST['prenom']
         email = request.POST['email']
         username = request.POST['username']
-        user_instance = User(name, surname, username, email)
+        user_instance = User(id, name, surname, username, email)
         user_instance.save()
     except Exception as e:
         return HttpResponse(e)
